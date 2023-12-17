eee38e5b7311ca32f96269b832ab797aaf2d9659
Maurice-Tam <142327189+Maurice-Tam@users.noreply.github.com>
2023-12-17 16:44:59 -0500

    1st FAC-01

--- src/main/java/com/example/demo/HomeController.java
+++ src/main/java/com/example/demo/HomeController.java
@@ -2,6 +2,7 @@ package com.example.demo;
 
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -11,4 +12,10 @@ public class HomeController {
     public String getHello() {
         return "Hello World!!!";
     }
+
+    @RequestMapping("/hello-world")
+    public String getHelloWorld() {
+        return "Hello World 2!!!";
+    }
+
 }
