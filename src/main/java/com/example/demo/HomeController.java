3e4a874853080795ad4fbac4e84554016af7ee24
Maurice-Tam <142327189+Maurice-Tam@users.noreply.github.com>
2024-02-02 16:18:02 -0500

    Update HomeController.java

--- src/main/java/com/example/demo/HomeController.java
+++ src/main/java/com/example/demo/HomeController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HomeController {
 
-    @GetMapping("/hello")
+    @GetMapping("/")
     public String getHello() {
         return "Hello World!!!";
     }
